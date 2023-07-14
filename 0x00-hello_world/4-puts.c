5c45ee636121a7df39f9abc7975dbdeee1fcafbe
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-07-13 18:47:56 -0700

    fix: betty error was fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int puts(const char *s);
-int main (void)
+/* This is the entry point */
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 	return (0);
