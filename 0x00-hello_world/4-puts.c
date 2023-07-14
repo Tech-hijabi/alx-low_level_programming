963ed9811924b37b0100694597048427e92c41b7
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-07-13 19:15:51 -0700

    fix: betty-style and betty-doc usage for function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-int puts(const char *s);
-/* This is the entry point */
+/**
+ * main - This is the entry point
+ *
+ * Return: 0 (for success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
