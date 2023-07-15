38106c8bc98e9837641e539e3f0fedafe677a4f5
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-07-14 20:03:37 -0700

    printing digits in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int digit = '0';
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
