328bb1b4ef6ce33d9acb1061d1114fbfa6d15179
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-07-17 19:57:55 -0700

    still fixing

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,35 +11,24 @@ int main(void)
 {
 	int m = 0;
 	int n = 0;
-	int o = 0;
-	int p = 1;
 
-	while (m <= 9)
+	while (m <= 99)
 	{
-		while (n <= 9)
+		while (n <= 99)
 		{
-			while (o <= 9)
+			if (m < n)
 			{
-				while (p <= 9)
-				{
-					if (m != n || o != p)
-					{
-						putchar(m + 48);
-						putchar(n + 48);
-						putchar(' ');
-						putchar(o + 48);
-						putchar(p + 48);
+				putchar((m / 10) + 48);
+				putchar((m % 10) + 48);
+				putchar(' ');
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
 
-						if (m != 9 || n != 8 && o != 9 || p != 9)
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-					p++;
+				if (m != 98 || n != 99)
+				{
+					putchar(',');
+					putchar(' ');
 				}
-				p = o + 1;
-				o++;
 			}
 			n++;
 		}
