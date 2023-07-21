7b27f2643d4139a2a882144f5cf31c586162d3e3
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-07-21 16:00:29 -0700

    quick fix on requirement

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,14 +13,9 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-
-	if (n < 10)
-	{
-		_putchar(n + '0');
-	}
-	else
+	if (n / 10)
 	{
 		print_number(n / 10);
-		_putchar(n % 10 + '0');
 	}
+	_putchar(n % 10 + '0');
 }
