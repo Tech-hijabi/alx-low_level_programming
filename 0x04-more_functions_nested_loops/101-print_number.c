d350c610eeeec69b5324bb5357ee9a50eda4356a
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-07-21 16:31:22 -0700

    quick Fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,16 +8,19 @@
  */
 void print_number(int n)
 {
-	unsigned int a = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		a = -a;
+		n = -n;
 	}
-	if (a / 10)
+	if (n < 10)
 	{
-		print_number(a / 10);
+		_putchar(n + '0');
 	}
-	_putchar(a % 10 + '0');
+	else
+	{
+		print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
 }
+
