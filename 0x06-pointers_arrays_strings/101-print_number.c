28084102d330dee392b05e1375ca6e0fec50e523
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-07-28 19:56:50 -0700

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - function prints an integer
+ * @n: represents the numbers given in the main function
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar(45);
+		n = -n;
+	}
+	if (n < 10)
+	{
+		_putchar(n + '0');
+	}
+	else
+	{
+		print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
+}
