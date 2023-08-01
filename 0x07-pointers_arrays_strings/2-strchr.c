33bd5cdf195d1d5ec623f10e8fb0d8b7ed3537c4
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-07-31 17:23:21 -0700

    Task 2 on locating character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "main.h"
 
 /**
@@ -11,7 +12,7 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i] >= '\0')
 	{
 		if (s[i] == c)
 		{
@@ -19,5 +20,5 @@ char *_strchr(char *s, char c)
 		}
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
