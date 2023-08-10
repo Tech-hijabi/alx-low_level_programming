7def773d869c36f92336ebf1555ac3edbf94baf0
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-08-09 17:46:10 -0700

    Task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * strtow - function splits a string into words
+ * @str: the string that is splitted
+ *
+ * Return: NULL (if failure)
+ */
+char **strtow(char *str)
+{
+	if (str == NULL || str == "")
+		return (NULL);
+}
