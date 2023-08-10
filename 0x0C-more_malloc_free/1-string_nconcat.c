5f238058dd0bdd16d6ef722831a8ff5b8acc66e8
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-08-10 09:59:27 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,8 +15,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len1, len2;
 	char *conc_str;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
