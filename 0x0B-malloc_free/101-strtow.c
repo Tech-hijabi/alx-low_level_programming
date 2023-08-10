825862eca09ae9241151be0f7114ed8bc62214e9
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-08-09 18:19:02 -0700

    Task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "main.h"
 
+int count_words(char *str);
+
 /**
  * strtow - function splits a string into words
  * @str: the string that is splitted
@@ -9,6 +11,55 @@
  */
 char **strtow(char *str)
 {
-	if (str == NULL || str == "")
+	int i;
+	char **words, *start;
+
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
+
+	words = malloc(sizeof(char *) * (count_words(str) + 1));
+
+	if (words == NULL)
+		return (NULL);
+
+	for (i = 0; i < count_words(str) + 1; i++)
+	{
+		words[i] = NULL;
+	}
+	i = 0;
+	start = str;
+
+	while (i < count_words(str))
+	{
+		while (*start == ' ')
+			start++;
+		if (*start == '\0')
+			break;
+		words[i] = start;
+		while (*start != ' ' && *start != '\0')
+			start++;
+		*start = '\0';
+		i++;
+	}
+	return (words);
+}
+
+
+/**
+ * count_words - function counts numbers words the string is splitted into
+ * @str: the string
+ *
+ * Return: the count
+ */
+int count_words(char *str)
+{
+	int i, count = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+			count++;
+	}
+	count++;
+	return (count);
 }
