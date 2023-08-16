a5d7fd0351938b0c31a6a153157184e3ff87d3d8
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-08-16 14:25:03 -0700

    Task 0 is about calling the function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function prints a name
+ * @name: the name to be printed
+ * @f: pointer to the function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
