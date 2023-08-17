2433f94e80e55c529702ad1c3c44c0b575373e7a
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-08-17 07:29:48 -0700

    Printing numbers with a separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
+#include <stddef.h>
 /**
  * print_numbers - function prints numbers
  * @separator: pointer to the string to be printed between numbers
@@ -16,7 +17,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d%s", va_arg(num, int), i < n - 1 ? separator : "");
+		printf("%d", va_arg(num, int));
+
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
 	}
 	va_end(num);
 	printf("\n");
