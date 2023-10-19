60ab2ba7bbaed28f349bb4d25c84cad3fbeb5d13
Barakat Mohammed <adeyemoadeshewa10@gmail.com>
2023-10-19 16:07:21 -0700

    red-checks fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,12 +4,13 @@
  * error_exit -  function exits with an error upon failure
  * @exit_code: code number of the exit
  * @message: the message printed when error is encountered
+ * @filename: name of the file involved
  *
  * exit: exit_code
  */
-void error_exit(int exit_code, const char *message)
+void error_exit(int exit_code, const char *message, const char *filename)
 {
-	dprintf(STDERR_FILENO, "%s\n", message);
+	dprintf(STDERR_FILENO, message, filename);
 	exit(exit_code);
 }
 
@@ -30,18 +31,18 @@ int main(int argc, char *argv[])
 	ssize_t bytes_read, bytes_written;
 
 	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to");
+		error_exit(97, "Usage: cp file_from file_to", "");
 	file_from = argv[1];
 	file_to = argv[2];
 	from_f1 = open(file_from, O_RDONLY);
 	if (from_f1 == -1)
-		error_exit(98, "Error: Can't read from file %s");
+		error_exit(98, "Error: Can't read from file %s\n", file_from);
 	to_f2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
 			| S_IRGRP | S_IWGRP | S_IROTH);
 	if (to_f2 == -1)
 	{
 		close(from_f1);
-		error_exit(99, "Error: Can't write to file %s");
+		error_exit(99, "Error: Can't write to file %s\n", file_to);
 	}
 	while ((bytes_read = read(from_f1, buffer, sizeof(buffer))) > 0)
 	{
@@ -50,18 +51,18 @@ int main(int argc, char *argv[])
 		{
 			close(from_f1);
 			close(to_f2);
-			error_exit(99, "Error: Can't write to file %s");
+			error_exit(99, "Error: Can't write to file %s\n", file_to);
 		}
 	}
 	if (bytes_read == -1)
 	{
 		close(from_f1);
 		close(to_f2);
-		error_exit(98, "Error: Can't read from file %s");
+		error_exit(98, "Error: Can't read from file %s\n", file_from);
 	}
 	if (close(from_f1) == -1)
-		error_exit(100, "Error: Can't close fd");
+		error_exit(100, "Error: Can't close fd %s\n", file_from);
 	if (close(to_f2) == -1)
-		error_exit(100, "Error: Can't close fd");
+		error_exit(100, "Error: Can't close fd %s\n", file_to);
 	return (0);
 }
